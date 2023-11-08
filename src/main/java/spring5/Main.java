5f48253478e1d4c2389cb29ec33c323655c8f0e3
kjh940222 <kjh940222@naver.com>
2023-11-08 12:44:30 +0900

    Initial commit

---
+++ src/main/java/spring5/Main.java
@@ -0,0 +1,8 @@
+package spring5;
+
+public class Main {
+    public static void main(String[] args) {
+        TransportationWalk transportationWalk = new TransportationWalk();
+        transportationWalk.move();
+    }
+}
